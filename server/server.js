192c2352ac3feb4f00bd985ba418d4705611e222
Jurgita <jurgita.eicinaite@yahoo.com>
2023-09-05 19:54:48 +0300

    server refactor

--- server/server.js
+++ server/server.js
@@ -4,13 +4,6 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
-
 app.use(express.json());
 
 app.post('/search', (req, res) => {
